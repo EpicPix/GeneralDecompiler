bfa7b0eeb86389d7019d959a77d91795a425e4d4
EpicPix <epicpixminecraft@gmail.com>
2023-04-05 18:50:59 +0200

    Remove stestarch

--- src/arch/arch.c
+++ src/arch/arch.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-extern const arch_info arch_stestarch;
-
 const arch_info* arch_get(const char* name) {
-  if(strcmp(name, "stestarch") == 0) return &arch_stestarch;
   return NULL;
 }
