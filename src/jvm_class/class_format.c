f666be444fcdf2005a5f1525378edc92704c3ffb
EpicPix <epicpixminecraft@gmail.com>
2023-04-16 13:44:56 +0200

    Read the class file constant pool

--- src/jvm_class/class_format.c
+++ src/jvm_class/class_format.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "class_format.h"
 #include "../byte_reader.h"
 
@@ -13,6 +14,71 @@ static void* arch_load_data(uint8_t* code, int code_length) {
   cf->minor_version = read_word_be(code, &index, code_length);
   cf->major_version = read_word_be(code, &index, code_length);
   ARCH_LOG("Class file version: %d.%d", cf->major_version, cf->minor_version);
+
+  cf->constant_pool_count = read_word_be(code, &index, code_length) - 1;
+  ARCH_LOG("Constant pool entry count: %d", cf->constant_pool_count);
+
+  cf->constant_pool_entries = malloc(cf->constant_pool_count * sizeof(struct jvm_class_loaded_constant_pool_entry));
+  for(int i = 0; i<cf->constant_pool_count; i++) {
+    struct jvm_class_loaded_constant_pool_entry* entry = &cf->constant_pool_entries[i];
+    entry->tag = read_byte(code, &index, code_length);
+    switch(entry->tag) {
+    case 1:
+      entry->entry.utf8.length = read_word_be(code, &index, code_length);
+      entry->entry.utf8.bytes = malloc(entry->entry.utf8.length);
+      if(index + entry->entry.utf8.length > code_length) {
+        fprintf(stderr, "Cannot read %d bytes from index %d\n", entry->entry.utf8.length, index);
+        exit(1);
+      }
+      memcpy(entry->entry.utf8.bytes, code + index, entry->entry.utf8.length);
+      index += entry->entry.utf8.length;
+      break;
+    case 3:
+      entry->entry.u4.num = read_int_be(code, &index, code_length);
+      break;
+    case 4:
+      entry->entry.f4.num = read_float_be(code, &index, code_length);
+      break;
+    case 5:
+      entry->entry.u8.num = read_long_be(code, &index, code_length);
+      i++;
+      break;
+    case 6:
+      entry->entry.f8.num = read_double_be(code, &index, code_length);
+      i++;
+      break;
+    case 7:
+    case 8:
+    case 16:
+    case 19:
+    case 20:
+      entry->entry.index.index = read_word_be(code, &index, code_length);
+      break;
+    case 9:
+    case 10:
+    case 11:
+      entry->entry.ref.class_index = read_word_be(code, &index, code_length);
+      entry->entry.ref.name_and_type_index = read_word_be(code, &index, code_length);
+      break;
+    case 12:
+      entry->entry.name_and_type.name_index = read_word_be(code, &index, code_length);
+      entry->entry.name_and_type.descriptor_index = read_word_be(code, &index, code_length);
+      break;
+    case 15:
+      entry->entry.method_handle.reference_kind = read_byte(code, &index, code_length);
+      entry->entry.method_handle.reference_index = read_word_be(code, &index, code_length);
+      break;
+    case 17:
+    case 18:
+      entry->entry.dynamic.bootstrap_method_attr_index = read_word_be(code, &index, code_length);
+      entry->entry.dynamic.name_and_type_index = read_word_be(code, &index, code_length);
+      break;
+    default:
+      fprintf(stderr, "Unrecognized constant pool entry tag: %d\n", entry->tag);
+      exit(1);
+    }
+  }
+
   return cf;
 }
 
--- src/jvm_class/class_format.h
+++ src/jvm_class/class_format.h
@@ -7,6 +7,52 @@
 struct jvm_class_loaded_file {
   uint16_t minor_version;
   uint16_t major_version;
+  int constant_pool_count;
+  struct jvm_class_loaded_constant_pool_entry* constant_pool_entries;
+};
+
+struct jvmclass_utf8_entry {
+  char* bytes;
+  uint16_t length;
+} __attribute__((packed, aligned(1)));
+struct jvmclass_name_and_type_entry {
+  uint16_t name_index;
+  uint16_t descriptor_index;
+};
+struct jvmclass_method_handle_entry {
+  uint16_t reference_index;
+  uint8_t reference_kind;
+};
+struct jvmclass_dynamic_entry {
+  uint16_t bootstrap_method_attr_index;
+  uint16_t name_and_type_index;
+};
+struct jvmclass_ref_entry {
+  uint16_t class_index;
+  uint16_t name_and_type_index;
+};
+struct jvmclass_index_entry { uint16_t index; };
+struct jvmclass_int_entry { int num; };
+struct jvmclass_float_entry { float num; };
+struct jvmclass_long_entry { long num; };
+struct jvmclass_double_entry { double num; };
+
+union jvmclass_entry {
+  struct jvmclass_utf8_entry utf8;
+  struct jvmclass_name_and_type_entry name_and_type;
+  struct jvmclass_method_handle_entry method_handle;
+  struct jvmclass_dynamic_entry dynamic;
+  struct jvmclass_ref_entry ref;
+  struct jvmclass_index_entry index;
+  struct jvmclass_int_entry u4;
+  struct jvmclass_float_entry f4;
+  struct jvmclass_long_entry u8;
+  struct jvmclass_double_entry f8;
+} __attribute__((packed, aligned(1)));
+
+struct jvm_class_loaded_constant_pool_entry {
+  union jvmclass_entry entry;
+  uint8_t tag;
 };
 
 #define ARCH_LOG(format, ...) DEBUG_LOG("[jvmclass] " format, __VA_ARGS__)
