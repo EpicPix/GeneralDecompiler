12b67868b1de5e530517a71b56f2fda4cc557268
EpicPix <epicpixminecraft@gmail.com>
2023-04-16 13:02:35 +0200

    Show major and minor version of class file

--- src/jvm_class/class_format.c
+++ src/jvm_class/class_format.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "class_format.h"
+#include "../byte_reader.h"
 
 static void* arch_load_data(uint8_t* code, int code_length) {
-  return NULL;
+  int index = 0;
+  if(read_int_be(code, &index, code_length) != 0xCAFEBABE) {
+    fprintf(stderr, "Invalid java magic number\n");
+    exit(1);
+  }
+  struct jvm_class_loaded_file* cf = malloc(sizeof(struct jvm_class_loaded_file));
+  cf->minor_version = read_word_be(code, &index, code_length);
+  cf->major_version = read_word_be(code, &index, code_length);
+  ARCH_LOG("Class file version: %d.%d", cf->major_version, cf->minor_version);
+  return cf;
 }
 
 static struct arch_prepared_data arch_prepare_data(void* loaded_data) {
--- src/jvm_class/class_format.h
+++ src/jvm_class/class_format.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <stdint.h>
 #include "../arch/arch.h"
 #include "../utils.h"
+
+struct jvm_class_loaded_file {
+  uint16_t minor_version;
+  uint16_t major_version;
+};
 
 #define ARCH_LOG(format, ...) DEBUG_LOG("[jvmclass] " format, __VA_ARGS__)
