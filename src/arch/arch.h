55930d729146b42b66f957ef55d4ed1a5b1d43cb
EpicPix <epicpixminecraft@gmail.com>
2023-04-18 17:20:07 +0200

    Remove details from ir and arch for now

--- src/arch/arch.h
+++ src/arch/arch.h
@@ -3,13 +3,8 @@
 #include <stdint.h>
 #include "../ir/IR.h"
 
-struct arch_prepared_data {
-  void* prepared_data;
-  void* details_data;
-};
-
 typedef void* (*arch_load_data_func)(uint8_t* code, int code_length);
-typedef struct arch_prepared_data (*arch_prepare_data_func)(void* loaded_data);
+typedef void* (*arch_prepare_data_func)(void* loaded_data);
 typedef struct ir_data* (*arch_generate_ir_func)(void* prepared_data);
 
 typedef struct arch_info {
--- src/ir/IR.c
+++ src/ir/IR.c
@@ -4,10 +4,10 @@ struct ir_data* ir_optimize(struct ir_data* data) {
   return data;
 }
 
-void ir_print_instructions(struct ir_data* data, void* details) {
+void ir_print_instructions(struct ir_data* data) {
 
 }
 
-void ir_print_decompiled(struct ir_data* data, void* details) {
+void ir_print_decompiled(struct ir_data* data) {
 
 }
