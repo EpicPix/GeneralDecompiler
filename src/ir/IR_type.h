5ab4e763920213c5055a116a7281a4fb807a046c
EpicPix <epicpixminecraft@gmail.com>
2023-04-23 20:53:09 +0200

    Update the ir type system

--- src/ir/IR_type.h
+++ src/ir/IR_type.h
@@ -11,30 +11,49 @@ enum ir_type_definition_type {
 };
 
 struct ir_type {
-  bool is_symbol : 1;
+  bool is_builtin : 1;
   union {
     uint32_t symbol_data;
     struct {
       uint16_t size;
       enum ir_type_definition_type type : 16;
-    } pre_defined;
+    } built_in;
   };
 };
 
-static struct ir_type ir_type_unknown = { .is_symbol = false, .pre_defined = { .size = 0, .type = ir_type_definition_type_unknown } };
-static struct ir_type ir_type_u8 = { .is_symbol = false, .pre_defined = { .size = 1, .type = ir_type_definition_type_int_unsigned } };
-static struct ir_type ir_type_i8 = { .is_symbol = false, .pre_defined = { .size = 1, .type = ir_type_definition_type_int_signed } };
-static struct ir_type ir_type_f8 = { .is_symbol = false, .pre_defined = { .size = 1, .type = ir_type_definition_type_float } };
-static struct ir_type ir_type_u16 = { .is_symbol = false, .pre_defined = { .size = 2, .type = ir_type_definition_type_int_unsigned } };
-static struct ir_type ir_type_i16 = { .is_symbol = false, .pre_defined = { .size = 2, .type = ir_type_definition_type_int_signed } };
-static struct ir_type ir_type_f16 = { .is_symbol = false, .pre_defined = { .size = 2, .type = ir_type_definition_type_float } };
-static struct ir_type ir_type_u32 = { .is_symbol = false, .pre_defined = { .size = 4, .type = ir_type_definition_type_int_unsigned } };
-static struct ir_type ir_type_i32 = { .is_symbol = false, .pre_defined = { .size = 4, .type = ir_type_definition_type_int_signed } };
-static struct ir_type ir_type_f32 = { .is_symbol = false, .pre_defined = { .size = 4, .type = ir_type_definition_type_float } };
-static struct ir_type ir_type_u64 = { .is_symbol = false, .pre_defined = { .size = 8, .type = ir_type_definition_type_int_unsigned } };
-static struct ir_type ir_type_i64 = { .is_symbol = false, .pre_defined = { .size = 8, .type = ir_type_definition_type_int_signed } };
-static struct ir_type ir_type_f64 = { .is_symbol = false, .pre_defined = { .size = 8, .type = ir_type_definition_type_float } };
+enum ir_type_composed_type {
+  ir_type_composed_type_struct = 0,
+  ir_type_composed_type_union = 1,
+};
+
+struct ir_type_composed {
+  enum ir_type_composed_type composed_type;
+  int type_count;
+  struct ir_type contained_types[];
+};
+
+struct ir_type_table {
+  int start_index;
+  int entry_count;
+  int allocated_count;
+  struct ir_type_composed** types;
+  struct ir_type_table* next;
+};
+
+static struct ir_type ir_type_unknown = { .is_builtin = true, .built_in = { .size = 0, .type = ir_type_definition_type_unknown } };
+static struct ir_type ir_type_u8 = { .is_builtin = true, .built_in = { .size = 8, .type = ir_type_definition_type_int_unsigned } };
+static struct ir_type ir_type_i8 = { .is_builtin = true, .built_in = { .size = 8, .type = ir_type_definition_type_int_signed } };
+static struct ir_type ir_type_f8 = { .is_builtin = true, .built_in = { .size = 8, .type = ir_type_definition_type_float } };
+static struct ir_type ir_type_u16 = { .is_builtin = true, .built_in = { .size = 16, .type = ir_type_definition_type_int_unsigned } };
+static struct ir_type ir_type_i16 = { .is_builtin = true, .built_in = { .size = 16, .type = ir_type_definition_type_int_signed } };
+static struct ir_type ir_type_f16 = { .is_builtin = true, .built_in = { .size = 16, .type = ir_type_definition_type_float } };
+static struct ir_type ir_type_u32 = { .is_builtin = true, .built_in = { .size = 32, .type = ir_type_definition_type_int_unsigned } };
+static struct ir_type ir_type_i32 = { .is_builtin = true, .built_in = { .size = 32, .type = ir_type_definition_type_int_signed } };
+static struct ir_type ir_type_f32 = { .is_builtin = true, .built_in = { .size = 32, .type = ir_type_definition_type_float } };
+static struct ir_type ir_type_u64 = { .is_builtin = true, .built_in = { .size = 64, .type = ir_type_definition_type_int_unsigned } };
+static struct ir_type ir_type_i64 = { .is_builtin = true, .built_in = { .size = 64, .type = ir_type_definition_type_int_signed } };
+static struct ir_type ir_type_f64 = { .is_builtin = true, .built_in = { .size = 64, .type = ir_type_definition_type_float } };
 
 static bool ir_equal_types(struct ir_type a, struct ir_type b) {
-  return a.is_symbol == b.is_symbol && a.symbol_data == b.symbol_data;
+  return a.is_builtin == b.is_builtin && a.symbol_data == b.symbol_data;
 }
