9fc0d46e2ac6ba09896be81ffff55bdd3ed42624
EpicPix <epicpixminecraft@gmail.com>
2023-04-26 11:28:32 +0200

    Create a high level instruction format

--- src/ir/IR_high_level.h
+++ src/ir/IR_high_level.h
@@ -3,6 +3,52 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-struct ir_instruction_high {
+#include "IR_type.h"
 
+struct ir_instruction_high;
+
+
+enum ir_instruction_high_location_type {
+  ir_instruction_high_location_type_immediate,
+  ir_instruction_high_location_type_register,
+  ir_instruction_high_location_type_address,
+  ir_instruction_high_location_type_instruction,
+};
+
+struct ir_instruction_high_location {
+  ir_type_t type;
+  enum ir_instruction_high_location_type location_type;
+  union {
+    uint64_t imm;
+    uint64_t reg;
+    uint64_t addr;
+    struct ir_instruction_high* instr;
+  } data;
+};
+
+enum ir_instruction_high_type {
+  ir_instruction_high_type_pop,
+  ir_instruction_high_type_push,
+  ir_instruction_high_type_add,
+};
+
+struct ir_instruction_high {
+  enum ir_instruction_high_type type;
+  union {
+    struct ir_instruction_high_data_oii {
+      struct ir_instruction_high_location output;
+      struct ir_instruction_high_location inputa;
+      struct ir_instruction_high_location inputb;
+    } oii;
+    struct ir_instruction_high_data_oi {
+      struct ir_instruction_high_location output;
+      struct ir_instruction_high_location input;
+    } oi;
+    struct ir_instruction_high_data_i {
+      struct ir_instruction_high_location input;
+    } i;
+    struct ir_instruction_high_data_o {
+      struct ir_instruction_high_location output;
+    } o;
+  } data;
 };
