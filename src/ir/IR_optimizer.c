9b0f54365431407321de0c5988c2358d5fd5d03b
EpicPix <epicpixminecraft@gmail.com>
2023-05-14 01:52:22 +0200

    Create `ir_optimize_location_mappings`

--- src/ir/IR_optimizer.c
+++ src/ir/IR_optimizer.c
@@ -16,7 +16,17 @@ static struct ir_instruction_low* ir_get_instruction(struct ir_instruction_list*
   return NULL;
 }
 
-struct ir_instruction_list* ir_optimize_body(struct ir_instruction_list* output, uint64_t current_location, struct ir_instruction_list* input_instructions) {
+struct ir_optimize_location_mapping {
+    struct ir_instruction_low_location_with_offset loc;
+    uint64_t reg;
+};
+
+struct ir_optimize_location_mappings {
+    struct ir_optimize_location_mapping mapping;
+    struct ir_optimize_location_mappings* next;
+};
+
+static struct ir_instruction_list* ir_optimize_body(struct ir_instruction_list* output, uint64_t current_location, struct ir_instruction_list* input_instructions, struct ir_optimize_location_mappings) {
   struct ir_instruction_low* current_instruction = ir_get_instruction(input_instructions, current_location);
   return ir_instruction_add_instruction_low(output, 1024, *current_instruction);
 }
@@ -28,11 +38,12 @@ struct ir_data ir_optimize(struct ir_data data) {
   struct ir_instruction_list* instructions_start = ir_instruction_create_list(NULL, 0x10000, 1024, false);
   struct ir_instruction_list* instructions_current = instructions_start;
   struct ir_instruction_list* instructions_in = data.instructions;
+  struct ir_optimize_location_mappings* mappings = NULL;
 
   while(instructions_in) {
     for(uint64_t i = 0; i<instructions_in->instruction_count; i++) {
       uint64_t ptr = instructions_in->start_address + i;
-      instructions_current = ir_optimize_body(instructions_current, ptr, instructions_in);
+      instructions_current = ir_optimize_body(instructions_current, ptr, instructions_in, mappings);
     }
     instructions_in = instructions_in->next;
   }
