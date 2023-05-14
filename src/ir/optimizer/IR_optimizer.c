a6bda08da13a5c6c53b75284e640a76990960df2
EpicPix <epicpixminecraft@gmail.com>
2023-05-14 18:34:25 +0200

    Move optimizing to a different folder

--- src/ir/optimizer/IR_optimizer.c
+++ src/ir/optimizer/IR_optimizer.c
@@ -1,5 +1,5 @@
-#include "IR.h"
-#include "../utils.h"
+#include "IR_optimizer.h"
+#include "../../utils.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -15,30 +15,6 @@ static struct ir_instruction_low* ir_get_instruction(struct ir_instruction_list*
   }
   return NULL;
 }
-
-struct ir_optimize_location_mapping {
-    struct ir_instruction_low_location_with_offset loc;
-    uint64_t reg;
-};
-
-struct ir_optimize_location_mappings {
-    struct ir_optimize_location_mapping mapping;
-    struct ir_optimize_location_mappings* next;
-};
-
-struct ir_optimize_register_usage_data {
-    struct ir_instruction_low_location assigned_data;
-    struct ir_instruction_low_location reg;
-    uint64_t count;
-    struct ir_optimize_register_usage_data* next;
-    bool has_assigned_data;
-};
-
-struct ir_optimize_data {
-    struct ir_optimize_location_mappings* mappings;
-    uint64_t mapping_count;
-    struct ir_optimize_register_usage_data* register_usage;
-};
 
 static struct ir_optimize_location_mapping* ir_optimize_get_or_create_mapping(struct ir_instruction_low_location_with_offset loc, struct ir_optimize_data* data) {
   if(data->mappings == NULL) {
@@ -139,8 +115,6 @@ static struct ir_instruction_list* ir_optimize_put_instruction(struct ir_instruc
   }
   return ir_instruction_add_instruction_low(output, 1024, instr);
 }
-
-typedef struct ir_instruction_list* (*ir_optimize_step_func_t)(struct ir_instruction_list* output, uint64_t current_location, struct ir_instruction_list* input_instructions, struct ir_optimize_data* data);
 
 static struct ir_instruction_list* ir_optimize_body_step1(struct ir_instruction_list* output, uint64_t current_location, struct ir_instruction_list* input_instructions, struct ir_optimize_data* data) {
   struct ir_instruction_low* instr = ir_get_instruction(input_instructions, current_location);
---
+++ src/ir/optimizer/IR_optimizer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../IR.h"
+
+struct ir_optimize_location_mapping {
+    struct ir_instruction_low_location_with_offset loc;
+    uint64_t reg;
+};
+
+struct ir_optimize_location_mappings {
+    struct ir_optimize_location_mapping mapping;
+    struct ir_optimize_location_mappings* next;
+};
+
+struct ir_optimize_register_usage_data {
+    struct ir_instruction_low_location assigned_data;
+    struct ir_instruction_low_location reg;
+    uint64_t count;
+    struct ir_optimize_register_usage_data* next;
+    bool has_assigned_data;
+};
+
+struct ir_optimize_data {
+    struct ir_optimize_location_mappings* mappings;
+    uint64_t mapping_count;
+    struct ir_optimize_register_usage_data* register_usage;
+};
+
+typedef struct ir_instruction_list* (*ir_optimize_step_func_t)(struct ir_instruction_list* output, uint64_t current_location, struct ir_instruction_list* input_instructions, struct ir_optimize_data* data);
