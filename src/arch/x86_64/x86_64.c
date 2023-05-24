12df95e48819fc5aba15ef435c1ab7d6942cc18f
EpicPix <epicpixminecraft@gmail.com>
2023-05-24 10:57:52 +0200

    Add more stub code to x86_64

--- src/arch/x86_64/x86_64.c
+++ src/arch/x86_64/x86_64.c
@@ -1,17 +1,28 @@
 #include "x86_64.h"
 
+struct arch_data {
+    uint8_t* bytes;
+    int byte_count;
+};
+
 static void* arch_load_data(uint8_t* bytes, int byte_count) {
-  return NULL;
+  struct arch_data* ad = malloc(sizeof(struct arch_data));
+  ad->bytes = bytes;
+  ad->byte_count = byte_count;
+  return ad;
 }
 
 static void* arch_prepare_data(void* loaded_data) {
-  return NULL;
+  return loaded_data;
 }
 
 static struct ir_data arch_generate_ir(void* prepared_data) {
+  struct arch_data* data = prepared_data;
+  struct ir_instruction_list* instructions = ir_instruction_create_list(NULL, 0x10000, 1024, ir_instruction_level_high);
+
   return (struct ir_data) {
     .instruction_level = ir_instruction_level_high,
-    .instructions = ir_instruction_create_list(NULL, 0x10000, 1024, ir_instruction_level_high),
+    .instructions = instructions,
     .symbol_table = ir_symbol_create_table(NULL),
     .type_table = NULL,
     .memory_page_start = NULL
