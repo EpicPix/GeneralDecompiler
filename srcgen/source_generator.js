cb2b1c12187bb8dd8ec5322148c98f3624aaba1b
EpicPix <epicpixminecraft@gmail.com>
2023-05-29 22:43:35 +0200

    Create a test file

--- srcgen/source_generator.js
+++ srcgen/source_generator.js
@@ -8,19 +8,26 @@ if(args.length < 2) {
     exit(1);
 }
 
-const input = args[0];
+const input = path.resolve(args[0]);
 const output = path.resolve(args[1]);
 
 function runSourceGenerator() {
-    const test = generateFile(".", "test");
+    const instructions_x86_64 = readFile("instructions_x86_64");
+    // generateFileWithData(".", "test.c", ``);
+}
 
-    test.close();
+function readFile(name) {
+    return JSON.parse(fs.readFileSync(input + "/" + name + ".json"));
+}
+
+function generateFileWithData(dir, name, data) {
+    generateFile(dir, name).writeln(data).close();
 }
 
 function generateFile(dir, name) {
     const fdir = output + "/" + dir + "/generated/";
     fs.mkdirSync(fdir, { recursive: true });
-    const fname = fdir + name + ".c";
+    const fname = fdir + name;
     const stream = fs.createWriteStream(fname);
     stream.writeln = function(data) {
         stream.write(data.toString());
